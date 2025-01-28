39685a4fc0f33170f9c9edb9b82e2c4fec851494
flores <contatoluizsampaio@gmail.com>
2025-01-28 10:42:27 -0300

    Add files via upload

---
+++ Arithmetic-Calculator.py
@@ -0,0 +1,26 @@
+Operação = input("Qual sera a operação?:")
+print("Digite os numeros:")
+x = float(input(""))
+b = float(input(""))
+if Operação == "x":
+ print( x ,"Multiplicado por", b)
+ Resultado = x * b
+ print("Resultado =", Resultado)
+
+if Operação == "+":   
+ print( x ,"Mais", b)
+ Resultado = x + b
+ print("Resultado =", Resultado)
+
+if Operação == "-":  
+ print( x ,"Menos", b)
+ Resultado = x - b
+ print("Resultado =", Resultado)
+
+if Operação == "/":
+ print( x ,"Divido por", b)
+ Resultado = x / b
+ print("Resultado =", Resultado)
+
+
+
